9dc5f08040bb928be2f52624a8a726817ffeed95
sj609011 <44671976+sj609011@users.noreply.github.com>
2018-11-01 18:10:11 +0530

    Add files via upload
    This project is related to the input function which ask the user his/her name then it responds to the input by adding some text to it...
    and the reverse of that input...

---
+++ task4.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print ("GCI IS GREAT...")
+name = input ("What Is Your Name?")
+print("Hello "+ name +  ",please to meet you!")
+a=name
+print("Hello "+ name +  ",Did you know that your name backwards is "+ a[::-1]+"?" )
+ 
